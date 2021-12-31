c8455f7f5145c9dd1d53f5cf2ee20ccb5a9f099c
Dastan <dastan.kubanuulu1@gmail.com>
2021-12-31 22:52:29 +0600

    make it better

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, url_for, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost/dastan'
+app.config['SQLALCHAMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    intro = db.Column(db.String(150), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+@app.route('/home')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template('posts.html', articles=articles)
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article = article)
+
+@app.route('/posts/<int:id>/del')
+def post_del(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        print('dasdasd')
+
+@app.route('/posts/<int:id>/red', methods = ['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            print('dasdasd')
+
+    else:
+        article = Article.query.get(id)
+        return render_template('post_update.html', article = article)
+
+
+
+@app.route('/create-article', methods = ['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title = title, intro = intro, text = text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print('dasdasd')
+
+    else:
+        return render_template('create-article.html')
+
+
+if __name__ == '__main__':
+    app.run()
