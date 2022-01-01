01c6c1e147123c1d795c63e8f49334a5b486deda
Dastan <dastan.kubanuulu1@gmail.com>
2022-01-01 17:04:31 +0600

    сортировка постов

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def index():
 
 @app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.date).all()
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
 
 @app.route('/posts/<int:id>')
