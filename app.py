da175ffdcfc2c0ea49a3f9b2a497a7baa37b48be
Dastan <dastan.kubanuulu1@gmail.com>
2022-01-02 20:13:33 +0600

    asd

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ijzlpuphreujlh:e11bef18740061b989440cde9c4a11a8241c2e5d65ee6ce0ae9590d528077bc8@ec2-63-34-223-144.eu-west-1.compute.amazonaws.com:5432/dc3ehfbft7bif'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost/dastan'
 # app.config['SQLALCHAMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
